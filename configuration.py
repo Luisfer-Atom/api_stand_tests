63c2383fa7b7fb3aed5587a0cd770f4e6f524108
Your Full Name <your.email@example.com>
2025-02-12 20:43:01 -0600

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,4 @@
+URL_SERVICE = "https://cnt-a81792e0-12dc-4e7f-9445-abcf0bbb5d45.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
---
+++ data.py
@@ -0,0 +1,23 @@
+headers = {
+    "Content-Type": "application/json"
+}
+
+user_body = {
+    "firstName": "Andrea",
+    "phone": "+11234567890",
+    "address": "123 Elm Street, Hilltop"
+}
+
+product_ids = {
+    "ids": [1, 2, 3]
+}
+
+
+# esta función cambia los valores en el parámetro "firstName"
+def get_user_body(first_name):
+    # el diccionario que contiene el cuerpo de solicitud se copia del archivo "data" (datos) para conservar los datos del diccionario de origen
+    current_body = data.user_body.copy()
+    # Se cambia el valor del parámetro firstName
+    current_body["firstName"] = first_name
+    # Se devuelve un nuevo diccionario con el valor firstName requerido
+    return current_body
